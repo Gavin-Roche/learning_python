bd36e7a4a1d47591b47755521504eea0e438015f
Gavin-Roche <cubergr@gmail.com>
2025-05-31 10:47:51 +0100

    Added Name_split.py

---
+++ Basic_concepts_projects/Name_split.py
@@ -0,0 +1,10 @@
+# Splits the input full name into first and last name
+
+name = str(input("Input your first and second name: "))
+spaceLoc = name.index(" ") 
+
+fname = name[:spaceLoc]
+lname = name[spaceLoc+1:]
+
+print(fname)
+print(lname)
