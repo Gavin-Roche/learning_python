8f0e0ae254a774e81c01342d08c6d81e2f9ad948
Gavin-Roche <cubergr@gmail.com>
2025-05-31 10:45:38 +0100

    Added Combine_names.py

---
+++ Basic_concepts_projects/Combine_names.py
@@ -0,0 +1,8 @@
+# Combines two lists of names (girls' and boys' names)
+
+girlsNames = ["Avril", "Linda", "Jane"]
+boysNames  = ["James", "Thomas", "Frank"]
+
+names = girlsNames + boysNames
+
+print(names)
