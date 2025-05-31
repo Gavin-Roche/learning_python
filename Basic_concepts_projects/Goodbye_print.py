b05d2e26c2904ddb4a9315e6324f7dbf48a4f372
Gavin-Roche <cubergr@gmail.com>
2025-05-31 10:46:42 +0100

    Added Goodbye_print.py

---
+++ Basic_concepts_projects/Goodbye_print.py
@@ -0,0 +1,4 @@
+# Prints two "Goodbye!" messages with different endings
+
+print("Goodbye!", end=" ")  # This will print "Goodbye!" and avoid a newline
+print("Goodbye!")  # This will print "Goodbye!" on the same line after a space
