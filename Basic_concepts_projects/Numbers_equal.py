9ee94e57bf516ca690b2127dc251f922fa41a8b3
Gavin-Roche <cubergr@gmail.com>
2025-06-01 20:25:54 +0100

    Added Numbers_equal.py

---
+++ Basic_concepts_projects/Numbers_equal.py
@@ -0,0 +1,9 @@
+#Returns whether two numbers are the same
+
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter a number:"))
+
+if num1 == num2:
+    print("The numbers are the same")
+else:
+    print("The numbers are not the same")
