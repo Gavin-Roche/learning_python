b9f62bb3c9ed55bb0e91dcfc99449282804613a7
Gavin-Roche <cubergr@gmail.com>
2025-06-01 20:30:32 +0100

    Added Turtle_intro.py

---
+++ Basic_concepts_projects/Turtle_intro.py
@@ -0,0 +1,19 @@
+import turtle
+
+#Create a turtle object called pen
+pen = turtle.Turtle()
+
+#Create a window for the turtle
+window = turtle.Screen()
+
+pen.forward(100) #move forword 100 units
+pen.left(90)     #turn left by 90 degrees
+pen.forward(100)
+pen.right(45)
+pen.forward(100)
+pen.left(90)
+pen.forward(99.292799999999999)
+
+#keep looping until window is closed
+window.mainloop()
+
