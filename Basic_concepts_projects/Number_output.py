fae78e3c4101aa16d054cac3f155772f57762bb3
Gavin-Roche <cubergr@gmail.com>
2025-06-01 20:25:33 +0100

    Added Number_output.py

---
+++ Basic_concepts_projects/Number_output.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number 1-3:"))
+#Check the value of the number
+if number == 1:
+    print("Number one")
+if number == 2:
+    print("Number two")
+if number== 3:
+    print("Number three")
