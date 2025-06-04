137ce32e79549ad81c34df9f36ef642682be1e63
Gavin-Roche <cubergr@gmail.com>
2025-06-04 11:19:51 +0100

    Added Distance_conversion.py

---
+++ Basic_projects/Distance_conversion.py
@@ -0,0 +1,8 @@
+# Converts distance from miles to kilometers and calculates flight cost
+
+distanceMiles = 7061
+distanceKm = 7061 * 1.60935
+pricePreKm = 700
+
+print("Distance from Fermi to Singapore in miles is", distanceMiles, "is equal to", round(distanceKm, 2))
+print("The cost to fly to Singapore at €", pricePreKm, "per Km is €", round(distanceKm * pricePreKm, 2))
