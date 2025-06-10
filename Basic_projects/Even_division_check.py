fdf76baa373c641cbc3c5b693c0c77df3f6aa2d8
Gavin-Roche <cubergr@gmail.com>
2025-06-10 21:12:14 +0100

    Added Even_division_check.py

---
+++ Basic_projects/Even_division_check.py
@@ -0,0 +1,9 @@
+# Check if one number divides evenly into another
+
+num1 = int(input("Input number 1: "))
+num2 = int(input("Input number 2: "))
+
+if num1 % num2 == 0:
+    print(num1, "divides evenly into", num2)
+else:
+    print(num1, "doesn't divide evenly into", num2)
