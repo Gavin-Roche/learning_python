330ccaf7c2de88c391519817c827327996ea1fbf
Gavin-Roche <cubergr@gmail.com>
2025-06-15 18:52:13 +0100

    Added Letter_count.py

---
+++ Basic_projects/Letter_count.py
@@ -0,0 +1,14 @@
+# This program counts the number of occurrences of a specified letter in a sentence
+
+sentence = input("Please enter sentence: ")
+findLetter = input("Please input the letter you want to find: ")
+numLetters = 0
+
+for char in sentence:
+    if char == findLetter:
+        numLetters += 1
+
+if numLetters == 0:
+    print("There is no", findLetter, "in your sentence")
+else:
+    print("The number of", findLetter, "in your sentence is", numLetters)
