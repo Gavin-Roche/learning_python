70caf3535e2284bfe6a878fa783abade4922b224
Gavin-Roche <cubergr@gmail.com>
2025-06-18 06:29:52 +0100

    Added Milk_cost_calculation.py

---
+++ Basic_projects/Milk_cost_calculation.py
@@ -0,0 +1,21 @@
+# This program calculates the total cost of milk based on hours spent in the house
+
+hours = []
+liters = []
+cost = float(input("Please enter the price of the milk: "))
+
+for i in range(7):
+    number = int(input("Please enter a number of hours Stephen was in the house: "))
+    hours.append(number)
+print(hours)
+
+for i in range(7):
+    entry = hours[i] / 2
+    liters.append(entry)
+print(liters)
+
+totalLiters = sum(liters)
+print(totalLiters)
+
+totalCost = totalLiters * cost
+print(round(totalCost, 2))
