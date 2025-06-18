68ff3fb94623b6c3abafb17c7b060081048e4e19
Gavin-Roche <cubergr@gmail.com>
2025-06-18 06:30:17 +0100

    Added Negative_to_zero.py

---
+++ Basic_projects/Negative_to_zero.py
@@ -0,0 +1,9 @@
+# Turns the negative numbers in the list to zero
+
+myList = [1, -19, 27, 8, -5, 9]
+print(myList)
+
+for item in range(len(myList)):
+    if myList[item] < 0:
+        myList[item] = 0
+print(myList)
