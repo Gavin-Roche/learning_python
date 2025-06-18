55828072656f822921ca5ab9b836f22581b330ff
Gavin-Roche <cubergr@gmail.com>
2025-06-18 06:31:09 +0100

    Added Pet_names.py

---
+++ Basic_projects/Pet_names.py
@@ -0,0 +1,14 @@
+# This program collects and displays the names of the user's pets
+
+petList = []
+totalPets = int(input("How many pets do you have: "))
+
+if totalPets > 0:
+    for pet in range(totalPets):
+        name = input("Enter pet's name: ")
+        petList.append(name)
+    print("The names of your pets are: ")
+    for pet in range(totalPets):
+        print(petList[pet])
+else:
+    print("Buy one so!!!")
