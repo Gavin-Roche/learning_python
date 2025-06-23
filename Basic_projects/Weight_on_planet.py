131689ce36c30e1ecd555a4ca811b2aba35b917e
Gavin-Roche <cubergr@gmail.com>
2025-06-23 15:57:00 +0100

    Added Weight_on_planet.py

---
+++ Basic_projects/Weight_on_planet.py
@@ -0,0 +1,17 @@
+# This program calculates the user's weight on either Mars or the Moon
+
+userWeight = float(input("input weight in kgs:"))
+
+planet = input("input moon or mars:")
+
+mars = 0.38
+moon = 0.165
+
+if planet == "mars":
+    newWeight = userWeight * mars
+elif planet == "moon":
+    newWeight = userWeight * moon
+else:
+    newWeight = "Invalid planet"
+
+print("This would weigh", newWeight, "Kgs on the", planet)
